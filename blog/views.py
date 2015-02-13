f1ec4a332c98e2ae02b8e49d1429420caf1f20ba
Jayadeep KM <kmjayadeep@gmail.com>
2015-02-13 16:41:05 +0530

    Articles page model complete

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from blog.models import Post
+from blog.models import Post,Comment,Label,PostLabels
 
 
 def index(request):
 	posts = Post.objects.all()
-	data = {'title':'Articles','posts':posts}
-	return render(request,'front/blog/index.html',data)
+
+	data = []
+	for post in posts:
+		com = Comment.objects.filter(post_id=post.id)
+		postlabel = PostLabels.objects.filter(post_id=post.id)
+		data.append({'post':post,'comments':com,'labels':postlabel})
+
+
+	return render(request,'front/blog/index.html',{'title':'Articles','data':data})
